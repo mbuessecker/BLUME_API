21781f7a2054cfa32be14141203669dd4b5e869a
mbuessecker <buessecker@posteo.de>
2019-04-12 16:38:51 +0200

    Get measurement types and timestamp from a measuring point

--- scraper.js
+++ scraper.js
@@ -12,7 +12,38 @@ rp(baseUrl + '/lqi')
         for (let i = 0; i < numberOfMeasuringPoints; i++) {
             urlsMeasuringPoints.push($('a.lmn-button', html)[i].attribs.href);
         }
+
+        // Now we can get the measurements from the measuring points.
+        urlsMeasuringPoints.forEach(measuringPointUrl => {
+            getLatestMeasurements(baseUrl + measuringPointUrl)
+        });
     })
     .catch(function (err) {
         //handle error
     });
+
+function getLatestMeasurements(measuringPointUrl) {
+    rp(measuringPointUrl)
+        .then((html) => {
+            // We need: latitude, longitude, (height), timestamp, measurements and measurementTypes 
+            const measurementTypesTableRow = $('thead', html).children().first().children();
+            const numberOfMeasurementsTypes = $(measurementTypesTableRow).length;
+            const latestMeasurementTableRow = $('tbody', html).children().first();
+            const dateTableCell = $(latestMeasurementTableRow).children().first();
+
+            let timestamp = $(dateTableCell).children().first().text();
+            timestamp = new Date(timestamp).toLocaleString();
+
+            const measurementTypes = [];
+            for (let i = 1; i < numberOfMeasurementsTypes; i++) {
+                const currentTableCell = $(measurementTypesTableRow[i]);
+                measurementTypes.push($(currentTableCell).children().first().text());
+            }
+            console.log(timestamp);
+            console.log(measurementTypes);
+
+            return {
+                'timestamp': timestamp
+            }
+        });
+}
