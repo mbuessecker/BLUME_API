4a0f42cc40665f0ae6d79522b15d822e5d16275c
mbuessecker <buessecker@posteo.de>
2019-04-12 20:24:22 +0200

    Add description of next task

--- server.js
+++ server.js
@@ -22,6 +22,27 @@ app.get('/api/v1/latestMeasurement_PM10', asyncMiddleware(async (req, res) => {
 app.listen(port, asyncMiddleware(async (req, res) => {
     console.log(`Listening on port ${port}`);
     await scraper.scrape().then(async data => {
-        console.log(data);
+        // Here we can access the scraped data from BLUME.
+        // console.log(data);
+        /* For every measuring point (Messstaion) there is a timestamp, the coordinated, and measurements. For the API we need to combine all the measurements for one measurement type (e.g. PM10) and format it so it looks like this: 
+        [
+            {
+                timestamp: ...,
+                latitude: ...,
+                longitude: ...,
+                measurement: ...(number)
+            },
+            {
+                timestamp: ...,
+                latitude: ...,
+                longitude: ...,
+                measurement: ...(number)
+            },
+            .
+            .
+            .
+        ]
+        I already added one endpoint above. Just add more endpoints for the other measurement types and send the corresponding data :)
+        */
     });
 }));
