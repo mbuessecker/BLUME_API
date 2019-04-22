083dbef643e10ed28b7aee664e87772c7fa17272
Melina Jeschar <Melina@Melinas-MacBook-Air.local>
2019-04-22 11:54:12 +0200

    Add GET /latestMeasurement endpoint

--- scraper.js
+++ scraper.js
@@ -8,7 +8,7 @@ const API_KEY_GOOGLE = config['API_KEY_GOOGLE'];
 
 const baseUrl = 'https://luftdaten.berlin.de';
 
-async function scrape() {
+async function scrape(requestedMeasurementTypes) {
     // First we have to get the URLs of the measuering points of BLUME.
     return rp(baseUrl + '/lqi')
         .then(async html => {
@@ -20,9 +20,9 @@ async function scrape() {
                 urlsMeasuringPoints.push($('a.lmn-button', html)[i].attribs.href);
             }
 
-            // Now we can get the measurements from the measuring points.
+            // Now we can get the measurements from the measuring points.            
             await Promise.all(urlsMeasuringPoints.map(async measuringPointUrl => {
-                data.push(await getLatestMeasurements(baseUrl + measuringPointUrl));
+                data.push(await getLatestMeasurements(baseUrl + measuringPointUrl, requestedMeasurementTypes));
             }));
             return data;
         })
@@ -31,45 +31,46 @@ async function scrape() {
         });
 }
 
-async function getLatestMeasurements(measuringPointUrl) {
+async function getLatestMeasurements(measuringPointUrl, requestedMeasurementTypes) {
     return rp(measuringPointUrl)
         .then(async html => {
+
+            let needsAllMeasurements = requestedMeasurementTypes.length == 0;
             // We need latitude, longitude, (height), timestamp, measurements and measurementTypes.
             const measurementTypesTableRow = $('thead', html).children().first().children();
             const numberOfMeasurementsTypes = $(measurementTypesTableRow).length;
             const latestMeasurementTableRow = $('tbody', html).children().first();
             const dateTableCell = $(latestMeasurementTableRow).children().first();
 
-            const measurementTypes = [];
-            for (let i = 1; i < numberOfMeasurementsTypes; i++) {
-                const currentTableCell = $(measurementTypesTableRow[i]);
-                measurementTypes.push($(currentTableCell).children().first().text());
-            }
-
-            const measurements = [];
-            $(dateTableCell).nextAll().each((i, element) => {
-                const measurementWithoutUnit = $(element).contents().not($(element).children()).text().trim();
-                measurements.push(measurementWithoutUnit);
-            });
+            var measurementValues = {};
 
             let timestamp = $(dateTableCell).children().first().text();
-            timestamp = new Date(timestamp).toLocaleString();
+            //timestamp = new Date(timestamp).toLocaleString();
 
             const address = $('dt:contains("Adresse:")', html).next().text();
             const coordinates = await getCoordinatesFromAddress(address);
 
-            const latestMeasurements = {
-                'timestamp': timestamp,
-                'latitude': coordinates.lat,
-                'longitude': coordinates.lng
-            }
-            measurementTypes.forEach((measurementType, i) => {
-                if (measurementType !== '') {
-                    latestMeasurements[measurementType] = measurements[i];
+            measurementValues["timestamp"] = timestamp;
+            measurementValues["latitude"] = coordinates.lat;
+            measurementValues["longitude"] = coordinates.lng;
+
+            requestedMeasurementTypes.forEach((i, element) => {
+                measurementValues[i] = null;
+            });
+
+            $(dateTableCell).nextAll().each((i, element) => {
+                const measurement = $(element).contents().not($(element).children()).text().trim();
+                const measurementUnit = $(measurementTypesTableRow[i]).children().first().text();
+                let isRequestedMeasurementUnit = requestedMeasurementTypes.includes(measurementUnit);
+
+                if ((needsAllMeasurements) || (isRequestedMeasurementUnit)) {
+                    if (measurementUnit !== '') {
+                        measurementValues[measurementUnit] = measurement;
+                    }
                 }
             });
 
-            return latestMeasurements;
+            return measurementValues;
         })
         .catch((err) => {
             console.log(err);
--- server.js
+++ server.js
@@ -15,15 +15,45 @@ const asyncMiddleware = fn =>
             .catch(next);
     };
 
-app.get('/api/v1/latestMeasurement_PM10', asyncMiddleware(async (req, res) => {
-    res.json('send Data');
+app.get('/api/v1/latestMeasurement', asyncMiddleware(async (req, res, next) => {
+    var parameters = [];
+    if(req.query.type !== undefined) {
+         //DO SOMEHTING
+         parameters = req.query.type.split(',');
+    }
+
+    res.json(req.query.type);
+    await scraper.scrape(parameters).then(async data => {
+        // Here we can access the scraped data from BLUME.
+         console.log(data);
+        /* For every measuring point (Messstaion) there is a timestamp, the coordinated, and measurements. For the API we need to combine all the measurements for one measurement type (e.g. PM10) and format it so it looks like this: 
+        [
+            {
+                timestamp: ...,
+                latitude: ...,
+                longitude: ...,
+                measurement: ...(number)
+            },
+            {
+                timestamp: ...,
+                latitude: ...,
+                longitude: ...,
+                measurement: ...(number)
+            },
+            .
+            .
+            .
+        ]
+        I already added one endpoint above. Just add more endpoints for the other measurement types and send the corresponding data :)
+        */
+    });
 }));
 
 app.listen(port, asyncMiddleware(async (req, res) => {
-    console.log(`Listening on port ${port}`);
-    await scraper.scrape().then(async data => {
+    //console.log(`Listening on port ${port}`);
+    await scraper.scrape([]).then(async data => {
         // Here we can access the scraped data from BLUME.
-        // console.log(data);
+         //console.log(data);
         /* For every measuring point (Messstaion) there is a timestamp, the coordinated, and measurements. For the API we need to combine all the measurements for one measurement type (e.g. PM10) and format it so it looks like this: 
         [
             {
